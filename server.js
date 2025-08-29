2900cd0de6cc02e722f0c66d0beaa134d980b2d6
tal0311 <tal.amit0311@gmail.com>
2025-08-29 19:06:36 +0300

    config: preview app cors update

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ if (process.env.NODE_ENV === 'production') {
             'http://127.0.0.1:5173',
             'http://localhost:5174',
             'http://127.0.0.1:5174',
+            'http://localhost:4173',
+            'http://127.0.0.1:4173',
         ],
         credentials: true,
     }
