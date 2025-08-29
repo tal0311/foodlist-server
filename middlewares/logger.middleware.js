213faea1715139a816984565f3ea3c6177d90849
tal0311 <tal.amit0311@gmail.com>
2025-08-29 18:16:35 +0300

    simple debug logger

--- middlewares/logger.middleware.js
+++ middlewares/logger.middleware.js
@@ -1,7 +1,14 @@
 import {logger} from '../services/logger.service.js'
 
 export async function log(req, res, next) {
-  logger.info('Sample Logger Middleware')
+  console.log('===Request===');
+  console.log('req.body', req.body)
+  console.log('req.params', req.params)
+  console.log('req.query', req.query)
+  console.log('req.method', req.method)
+  console.log('req.originalUrl', req.originalUrl)
+  console.log('===Request End===');
+  
   next()
 }
 
